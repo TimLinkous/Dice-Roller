b24410face5a8e9f5b860340110a4e29de28070a
Tim L <timl@Zipcoders-MacBook-Pro.local>
2024-07-16 21:36:23 -0400

    done with some bugs

--- roll.py
+++ roll.py
@@ -1,3 +1,5 @@
+import random
+
 # roll a "die" some number of times.
 # roll - run it once and go into a loop
 # roll 1 - produce a number 1-6 random and print it
@@ -5,3 +7,21 @@
 # roll 3 - produce three numbers and print them.
 #
 
+def roll_die(num_rolls):
+    result = [random.randint(1, 6) for _ in range(num_rolls)]
+    print(f"You rolled a {result}")
+
+def main():
+    while True:
+        user_input = input("Enter the number of dice you would like to roll: ")
+        try:
+            num_rolls = int(user_input) 
+            roll_die(num_rolls)
+        except ValueError:
+            print("Please enter a valid number")
+        
+
+
+if __name__ == "__main__":
+    main()
+        
